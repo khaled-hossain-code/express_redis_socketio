c354c9fbead5fff972613cb7d16f4064777641e6
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2019-02-06 09:11:16 +0600

    using ioredis as redis library

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,9 +1,8 @@
-const redis = require('redis');
-const bluebird = require('bluebird');
-const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+const Redis = require('ioredis');
+const redis = new Redis({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
+  db: 0
+});
 
-bluebird.promisifyAll(redis);
-
-module.exports = {
-  redisClient
-}
+module.exports = redis;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const {
-  redisClient
-} = require('./config/redis');
+const redis = require('./config/redis');
 
-// redisClient.set('COUNT', 0);
+redis.hmset('user:1',{
+  name: 'john',
+  age: 20,
+  gender: 'male',
+  factory: 1  
+});
+
+redis.hmset('user:2',{
+  name: 'jane',
+  age: 19,
+  gender: 'female',
+  factory: 1  
+});
+
+redis.hmset('user:3',{
+  name: 'doe',
+  age: 20,
+  gender: 'female',
+  factory: 1
+});
+
+redis.set('user:name:john', 'user:1');
+redis.set('user:name:jane', 'user:2');
+
+// redis.lpush('user:age:20', ['user:1', 'user:3']); //lpush will create the namespace and push it. if it exist then again push it
+// redis.lpush('user:age:19', 'user:2'); //if it runs twice then same data will be pushed twice
+
 
 app.get('/', async (req, res) => {
-  redisClient.incr('COUNT');
+  redis.incr('COUNT');
   try {
-    let count = await redisClient.getAsync('COUNT');
+    let count = await redis.get('COUNT');
     res.send(count);
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+app.get('/user/:userId', async (req, res) => {
+  try {
+    let user = await redis.hgetall(`user:${req.params.userId}`);
+    return res.send(user);
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+app.get('/user/name/:name', async (req, res) => {
+  try {
+    let userId = await redis.get(`user:name:${req.params.name}`);
+    let user = await redis.hgetall(userId)
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error)
+  }
+})
+app.get('/user/age/:age', async (req, res) => {
+  try {
+    let userIdList = redis.lrange(`user:age:19`);
+    console.log(userIdList);
+    let users = await Promise.all(userIdList.map(redis.hgetallAsync));
+    res.send(users);
   } catch (error) {
     res.status(500).send(error)
   }
