533991a29f6d74e51e7ca4b82146b989910ee3cd
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2019-02-07 08:12:26 +0600

    add invalid user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,3 @@
 const verifyUser = (token, socket) => {
-  //verify the user, if verified then call store user method neither disconnect the socket
+  //verify the user, if verified then call store user method neither emitError invalid user
 }
